5df7d28f1def0433f4affd924f40a760bfcdedfc
Stephen <stephen@vamrs.com>
2021-12-14 15:01:47 +0800

    rootfs: preinstall_u_boot.sh: remove 'rk' label
    Add support for radxa-zero u-boot package.
    
    Signed-off-by: Stephen <stephen@vamrs.com>

--- rootfs/scripts/preinstall_u_boot.sh
+++ rootfs/scripts/preinstall_u_boot.sh
@@ -10,5 +10,5 @@ if [[ "$(id -u)" -ne "0" ]]; then
 fi
 
 mkdir -p /tmp/u-boot /usr/lib/u-boot-${SUBBOARD}
-dpkg -X /packages/${SUBBOARD}-rk-ubootimg*.deb /tmp/u-boot
+dpkg -X /packages/${SUBBOARD}-*ubootimg*.deb /tmp/u-boot
 cp -rv /tmp/u-boot/usr/lib/u-boot-${SUBBOARD}/* /usr/lib/u-boot-${SUBBOARD}
