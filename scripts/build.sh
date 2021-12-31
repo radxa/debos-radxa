12b6a737a5518f17d912cb61bd5b30ca69da6a9c
Stephen <stephen@vamrs.com>
2021-12-31 10:39:09 +0800

    boards: add radxa-e25 build
    Signed-off-by: Stephen <stephen@vamrs.com>

--- scripts/build.sh
+++ scripts/build.sh
@@ -19,6 +19,8 @@ usage() {
     echo "Options:"
     echo "  ./build.sh -c rk3566 -b radxa-e23 -m debian -d buster -v xfce4 -a arm64 -f gpt"
     echo "  ./build.sh -c rk3566 -b radxa-e23 -m ubuntu -d focal -v server -a arm64 -f gpt"
+    echo "  ./build.sh -c rk3568 -b radxa-e25 -m debian -d buster -v xfce4 -a arm64 -f gpt"
+    echo "  ./build.sh -c rk3568 -b radxa-e25 -m ubuntu -d focal -v server -a arm64 -f gpt"
     echo "  ./build.sh -c rk3568 -b rock-3a -m debian -d buster -v xfce4 -a arm64 -f gpt"
     echo "  ./build.sh -c rk3568 -b rock-3a -m ubuntu -d focal -v server -a arm64 -f gpt"
     echo "  ./build.sh -c s905y2 -b radxa-zero -m debian -d buster -v xfce4 -a arm64 -f mbr"
