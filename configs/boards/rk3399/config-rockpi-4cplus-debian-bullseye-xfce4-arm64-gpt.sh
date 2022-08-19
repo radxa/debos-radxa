e1b8d35c753c5c8ef523a53b6077840ff7804f3c
Stephen <stephen@radxa.com>
2022-08-19 18:39:36 +0800

    add rock 4b/4c+ debian11 support
    Signed-off-by: Stephen <stephen@radxa.com>

---
+++ configs/boards/rk3399/config-rockpi-4cplus-debian-bullseye-xfce4-arm64-gpt.sh
@@ -0,0 +1,67 @@
+#!/bin/bash
+
+CMD=`realpath $0`
+CONFIG_BOARD_DIR=`dirname $CMD`
+TOP_DIR=$(realpath $CONFIG_BOARD_DIR/../../../)
+echo "TOP DIR = $TOP_DIR"
+BUILD_DIR=$TOP_DIR/build
+[[ ! -d "$BUILD_DIR" ]] && mkdir -p $BUILD_DIR
+
+# env
+export CPU=rk3399
+export BOARD=rockpi-4cplus
+export MODEL=debian
+export DISTRO=bullseye
+export VARIANT=xfce4
+export ARCH=arm64
+export FORMAT=gpt
+export IMAGESIZE=3000MB
+
+# Add pre-installed packages for target system
+cat > $BUILD_DIR/${BOARD}-${MODEL}-${DISTRO}-${VARIANT}-${ARCH}-${FORMAT}-packages.list <<EOF
+radxa-add-overlay*.deb
+rockchip-overlay*.deb
+linux-headers-4.4.194-*-rk3399-rockchip*.deb
+linux-image-4.4.194-*-rk3399-rockchip*.deb
+linux-firmware-image-4.4.194-*-rk3399-rockchip*.deb
+rockpi4-dtbo*.deb
+brcm-patchram-plus1*.deb
+broadcom-wifibt-firmware*.deb
+resize-assistant*.deb
+EOF
+
+# Add yaml variable
+cat > $BUILD_DIR/${BOARD}-${MODEL}-${DISTRO}-${VARIANT}-${ARCH}-${FORMAT}-variable.yaml <<EOF
+{{- \$board := or .board "${BOARD}" -}}
+{{- \$architecture := or .architecture "${ARCH}" -}}
+{{- \$model :=  or .model "${MODEL}" -}}
+{{- \$suite := or  .suite "${DISTRO}" -}}
+{{- \$imagesize := or .imagesize "${IMAGESIZE}" -}}
+{{- \$bootpartitionend := or .bootpartitionend "1081343S" -}}
+{{- \$rootpartitionstart := or .rootpartitionstart "1081344S" -}}
+{{- \$apt_repo := or .apt_repo "radxa" -}}
+
+EOF
+
+# Add images yaml
+cat > $BUILD_DIR/${BOARD}-${MODEL}-${DISTRO}-${VARIANT}-${ARCH}-${FORMAT}-yaml.list <<EOF
+00_architecture.yaml
+01_debootstrap_debian.yaml
+02_partitions_rockchip.yaml
+03_filesystem_deploy.yaml
+20_packages_start.yaml
+21_packages_base.yaml
+21_packages_bluetooth.yaml
+21_packages_devel.yaml
+21_packages_libs_debian_bullseye.yaml
+21_packages_math.yaml
+21_packages_sound.yaml
+21_packages_utilities.yaml
+21_packages_net.yaml
+21_packages_xfce.yaml
+21_packages_web.yaml
+22_packages_end.yaml
+70_system_common_setup.yaml
+85_u_boot.yaml
+90_clean_rootfs.yaml
+EOF
