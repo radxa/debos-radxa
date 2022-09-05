07396e113828157a0fe87d696b1f1dbbee74dc15
Stephen Chen <stephen@radxa.com>
2022-09-05 12:09:49 +0800

    configs: rock 5b: install packages via APT
    Signed-off-by: Stephen Chen <stephen@radxa.com>

--- configs/boards/rk3588/config-rock-5b-debian-bullseye-xfce4-arm64-gpt.sh
+++ configs/boards/rk3588/config-rock-5b-debian-bullseye-xfce4-arm64-gpt.sh
@@ -19,36 +19,12 @@ export IMAGESIZE=4000MB
 
 # Add pre-installed packages for target system
 cat > $BUILD_DIR/${BOARD}-${MODEL}-${DISTRO}-${VARIANT}-${ARCH}-${FORMAT}-packages.list <<EOF
-radxa-add-overlay*.deb
-rockchip-overlay*.deb
-linux-headers-5.10.66*.deb
-linux-image-5.10.66*.deb
-intel-wifibt-firmware*.deb
-realtek-wifibt-firmware*.deb
-resize-assistant*.deb
-librga2_2.2.0-1_arm64.deb
-librga2-dbgsym_2.2.0-1_arm64.deb
-librga-dev_2.2.0-1_arm64.deb
-librockchip-mpp1_1.5.0-1_arm64.deb
-librockchip-mpp-dev_1.5.0-1_arm64.deb
-librockchip-vpu0_1.5.0-1_arm64.deb
-rockchip-mpp-demos_1.5.0-1_arm64.deb
-camera-engine-rkaiq*arm64.deb
-xorg-server-source_1.20.11-1_all.deb
-xserver-common_1.20.11-1_all.deb
-xserver-xorg-legacy_1.20.11-1_arm64.deb
-xserver-xephyr_1.20.11-1_arm64.deb
-xserver-xorg-core_1.20.11-1_arm64.deb
-xserver-xorg-dev_1.20.11-1_arm64.deb
-glmark2_0.1_arm64.deb
-glmark2-data_2021.02+ds-1_all.deb
-glmark2-es2-x11_2021.02+ds-1_arm64.deb
-chromium-x11_91.0.4472.164_arm64.deb
-libmali-valhall-g610-g6p0-x11_1.9-1_arm64.deb
+
 EOF
 
 # Add yaml variable
 cat > $BUILD_DIR/${BOARD}-${MODEL}-${DISTRO}-${VARIANT}-${ARCH}-${FORMAT}-variable.yaml <<EOF
+{{- \$cpu :=  or .cpu "${CPU}" -}}
 {{- \$board := or .board "${BOARD}" -}}
 {{- \$architecture := or .architecture "${ARCH}" -}}
 {{- \$model :=  or .model "${MODEL}" -}}
@@ -69,15 +45,22 @@ cat > $BUILD_DIR/${BOARD}-${MODEL}-${DISTRO}-${VARIANT}-${ARCH}-${FORMAT}-yaml.l
 20_packages_start.yaml
 21_packages_base.yaml
 21_packages_bluetooth.yaml
+21_packages_camera.yaml
 21_packages_devel.yaml
-21_packages_libs_debian_bullseye.yaml
+21_packages_libs.yaml
+21_packages_graphics.yaml
+21_packages_gstreamer.yaml
+21_packages_kernel.yaml
 21_packages_math.yaml
+21_packages_mpp.yaml
 21_packages_mpv.yaml
 21_packages_sound.yaml
 21_packages_utilities.yaml
 21_packages_net.yaml
-21_packages_xfce.yaml
 21_packages_web.yaml
+21_packages_wifibt.yaml
+21_packages_xfce.yaml
+21_packages_xserver.yaml
 22_packages_end.yaml
 70_system_common_setup.yaml
 85_u_boot_rk35xx.yaml
